5f39ae29013dba42e1c79ecd37dc2683cb7a15b5
pythonlouvre <dbconfer@gmail.com>
2024-08-12 13:38:52 +0800

    番茄钟widget--保证后台运行倒计时精度

--- public/widgets/pomodoro-timer/script.js
+++ public/widgets/pomodoro-timer/script.js
@@ -1,6 +1,6 @@
 // script.js
 let startTime;
-let elapsedSeconds = 25 * 60 *1000; // 初始化为 25 分钟
+let elapsedSeconds = 25 * 60 *1000 * 1000; // 初始化为 25 分钟
 let intervalId;
 
 const timerDisplay = document.getElementById('timer-display');
@@ -12,8 +12,8 @@ const resetButton = document.getElementById('reset-button');
 const alarmSound = document.getElementById('alarm-sound');
 
 function updateTimerDisplay() {
-  minutesElement.textContent = `${Math.floor(elapsedSeconds / 60 / 1000)}`.padStart(2, '0');
-  secondsElement.textContent = `${elapsedSeconds % 60 /1000}`.padStart(2, '0');
+  minutesElement.textContent = `${Math.floor(elapsedSeconds / 60 / 1000/ 1000)}`.padStart(2, '0');
+  secondsElement.textContent = `${elapsedSeconds % 60 /1000/ 1000}`.padStart(2, '0');
 }
 
 function startTimer() {
@@ -22,7 +22,7 @@ function startTimer() {
   function tick() {
     const now = performance.now();
     const elapsed = now - startTime;
-    //console.log(elapsed);
+    console.log(elapsed);
     const remaining = elapsedSeconds - elapsed;
     console.log(remaining);
 
@@ -53,7 +53,7 @@ function stopTimer() {
 
 function resetTimer() {
   stopTimer();
-  elapsedSeconds = 25 * 60; // 重置为 25 分钟
+  elapsedSeconds = 25 * 60 * 1000 *1000; // 重置为 25 分钟
   updateTimerDisplay();
 }
 
